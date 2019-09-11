deb99a5b7883024b18e887700a588138027e3fae
Chiemeka-96 <davidilechukwu@gmail.com>
2019-09-11 05:26:32 +0100

    chatme

---
+++ ChatMe-master/app/src/main/java/com/nikitagordia/chatme/helper/MyApplication.java
@@ -0,0 +1,25 @@
+package com.nikitagordia.chatme.helper;
+
+import android.app.Application;
+
+import com.nikitagordia.chatme.R;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+
+/**
+ * Created by nikitagordia on 3/21/18.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
